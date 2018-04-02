9ebacbdd61d9d89227900da6e6ab100e9c18f386
Ken Corley <kenc138@gmail.com>
2018-04-02 15:49:13 -0600

    initial commit

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+# Provides access to getter/setter methods for database
+class Article < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+end
