9cd022de412c256f198cf10147e00224f9a57e2d
Ken Corley <kwc@Kens-MacBook-Pro.local>
2018-04-05 19:20:42 -0600

    Add debugger view, add database associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 # Provides access to getter/setter methods for database
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 2000 }
+  validates :user_id, presence: true
 end
