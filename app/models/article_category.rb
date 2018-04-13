14d29be86607654ed474507d95424df89e06149c
Ken Corley <kwc@Kens-MacBook-Pro.local>
2018-04-12 18:59:16 -0600

    Add article and category association

---
+++ app/models/article_category.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: article_categories
+#
+#  id          :integer          not null, primary key
+#  article_id  :integer
+#  category_id :integer
+#
+
+class ArticleCategory < ApplicationRecord
+  belongs_to :article
+  belongs_to :category
+end
